9617939437ecfadec16b7e35b1590aec3dae0010
laivannha <nguoikhongten02022005@gmail.com>
2026-09-10 22:39:48 +0700

    feat(complaints): wire file service for evidence previews

--- apps/api/src/modules/khieu-nai/khieu-nai.module.ts
+++ apps/api/src/modules/khieu-nai/khieu-nai.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { PhanQuyenModule } from '../phan-quyen/phan-quyen.module';
+import { TepTinModule } from '../tep-tin/tep-tin.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
 
 import { KhieuNaiQuanTriController } from './khieu-nai-quan-tri.controller';
@@ -8,7 +9,7 @@ import { KhieuNaiController } from './khieu-nai.controller';
 import { KhieuNaiService } from './khieu-nai.service';
 
 @Module({
-  imports: [XacThucModule, PhanQuyenModule],
+  imports: [XacThucModule, PhanQuyenModule, TepTinModule],
   controllers: [KhieuNaiController, KhieuNaiQuanTriController],
   providers: [KhieuNaiService],
   exports: [KhieuNaiService],
