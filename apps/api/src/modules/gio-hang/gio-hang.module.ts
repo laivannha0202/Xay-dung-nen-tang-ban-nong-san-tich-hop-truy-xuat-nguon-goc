169f1411265bb4d6766e06b25d9cabe553e8c7b1
laivannha <nguoikhongten02022005@gmail.com>
2026-09-10 21:36:18 +0700

    feat(cart): wire file service for product cover URLs

--- apps/api/src/modules/gio-hang/gio-hang.module.ts
+++ apps/api/src/modules/gio-hang/gio-hang.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { TepTinModule } from '../tep-tin/tep-tin.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
 
 import { CheckoutPreviewService } from './checkout-preview.service';
@@ -8,7 +9,7 @@ import { GioHangController } from './gio-hang.controller';
 import { GioHangService } from './gio-hang.service';
 
 @Module({
-  imports: [XacThucModule],
+  imports: [XacThucModule, TepTinModule],
   controllers: [GioHangController],
   providers: [GioHangService, CheckoutPricingService, CheckoutPreviewService],
   exports: [GioHangService, CheckoutPricingService],
