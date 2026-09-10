d55ebff978529ac3e2b011d448f01efba1960b1e
laivannha <nguoikhongten02022005@gmail.com>
2026-09-10 22:05:21 +0700

    feat(wishlist): wire file service for product covers

--- apps/api/src/modules/wishlist/wishlist.module.ts
+++ apps/api/src/modules/wishlist/wishlist.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { TepTinModule } from '../tep-tin/tep-tin.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
 
 import { WishlistController } from './wishlist.controller';
 import { WishlistService } from './wishlist.service';
 
 @Module({
-  imports: [XacThucModule],
+  imports: [XacThucModule, TepTinModule],
   controllers: [WishlistController],
   providers: [WishlistService],
 })
