6e4fa3d050b16daceaa5d2b6588bc3f65afbdf6c
laivannha <nguoikhongten02022005@gmail.com>
2026-09-10 21:02:28 +0700

    refactor(mobile): remove legacy Home presentation

--- apps/mobile/src/components/home/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './home-header';
-export * from './search-bar';
-export * from './hero-banner';
-export * from './quick-actions';
-export * from './category-grid';
-export * from './home-lower-sections';
-export * from './smart-product-image';
