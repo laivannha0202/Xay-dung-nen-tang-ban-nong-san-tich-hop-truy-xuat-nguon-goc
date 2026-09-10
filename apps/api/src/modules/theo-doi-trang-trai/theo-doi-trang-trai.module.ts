c65da9a5c84be2a9c97c06be3d89f9709641a159
laivannha <nguoikhongten02022005@gmail.com>
2026-09-10 22:26:43 +0700

    feat(farms): wire file service for followed farm images

--- apps/api/src/modules/theo-doi-trang-trai/theo-doi-trang-trai.module.ts
+++ apps/api/src/modules/theo-doi-trang-trai/theo-doi-trang-trai.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { TepTinModule } from '../tep-tin/tep-tin.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
 
 import { TheoDoiTrangTraiController } from './theo-doi-trang-trai.controller';
 import { TheoDoiTrangTraiService } from './theo-doi-trang-trai.service';
 
 @Module({
-  imports: [XacThucModule],
+  imports: [XacThucModule, TepTinModule],
   controllers: [TheoDoiTrangTraiController],
   providers: [TheoDoiTrangTraiService],
   exports: [TheoDoiTrangTraiService],
