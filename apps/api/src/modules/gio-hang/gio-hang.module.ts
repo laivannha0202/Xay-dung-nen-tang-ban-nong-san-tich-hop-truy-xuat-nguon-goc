c15b188f4fe033e803a726fa730ef3f3206a4125
laivannha <nguoikhongten02022005@gmail.com>
2026-09-11 08:05:27 +0700

    fix(checkout): wire promotion service into cart module

--- apps/api/src/modules/gio-hang/gio-hang.module.ts
+++ apps/api/src/modules/gio-hang/gio-hang.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { KhuyenMaiModule } from '../khuyen-mai/khuyen-mai.module';
 import { TepTinModule } from '../tep-tin/tep-tin.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
 
@@ -9,7 +10,7 @@ import { GioHangController } from './gio-hang.controller';
 import { GioHangService } from './gio-hang.service';
 
 @Module({
-  imports: [XacThucModule, TepTinModule],
+  imports: [XacThucModule, TepTinModule, KhuyenMaiModule],
   controllers: [GioHangController],
   providers: [GioHangService, CheckoutPricingService, CheckoutPreviewService],
   exports: [GioHangService, CheckoutPricingService],
