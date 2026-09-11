512e9ed214243a73f026b1a1adc68206d8b7e293
laivannha <nguoikhongten02022005@gmail.com>
2026-09-11 08:07:20 +0700

    feat(order): wire promotion and loyalty modules

--- apps/api/src/modules/don-hang/don-hang.module.ts
+++ apps/api/src/modules/don-hang/don-hang.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { DiemThuongModule } from '../diem-thuong/diem-thuong.module';
 import { GioHangModule } from '../gio-hang/gio-hang.module';
+import { KhuyenMaiModule } from '../khuyen-mai/khuyen-mai.module';
 import { PhanQuyenModule } from '../phan-quyen/phan-quyen.module';
 import { TonKhoModule } from '../ton-kho/ton-kho.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
@@ -12,7 +14,14 @@ import { DongGoiController } from './dong-goi.controller';
 import { DongGoiService } from './dong-goi.service';
 
 @Module({
-  imports: [XacThucModule, PhanQuyenModule, GioHangModule, TonKhoModule],
+  imports: [
+    XacThucModule,
+    PhanQuyenModule,
+    GioHangModule,
+    TonKhoModule,
+    KhuyenMaiModule,
+    DiemThuongModule,
+  ],
   controllers: [DonHangController, DonHangQuanTriController, DongGoiController],
   providers: [DonHangService, DongGoiService],
   exports: [DonHangService],
