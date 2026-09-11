5a690a3c3b8a462d18f1bf7bcef55c0059e669d1
laivannha <nguoikhongten02022005@gmail.com>
2026-09-11 09:01:43 +0700

    feat: wire refund benefit reconciliation

--- apps/api/src/modules/thanh-toan/thanh-toan.module.ts
+++ apps/api/src/modules/thanh-toan/thanh-toan.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 
+import { DiemThuongModule } from '../diem-thuong/diem-thuong.module';
+import { KhuyenMaiModule } from '../khuyen-mai/khuyen-mai.module';
 import { PhanQuyenModule } from '../phan-quyen/phan-quyen.module';
 import { TonKhoModule } from '../ton-kho/ton-kho.module';
 import { XacThucModule } from '../xac-thuc/xac-thuc.module';
@@ -10,6 +12,7 @@ import { VnPaySandboxGateway } from './gateway/vnpay-sandbox.gateway';
 import { ThanhToanCallbackController } from './thanh-toan-callback.controller';
 import { ThanhToanCallbackService } from './thanh-toan-callback.service';
 import { ThanhToanHoanTienController } from './thanh-toan-hoan-tien.controller';
+import { ThanhToanHoanTienHauXuLyService } from './thanh-toan-hoan-tien-hau-xu-ly.service';
 import { ThanhToanHoanTienService } from './thanh-toan-hoan-tien.service';
 import { ThanhToanTaiChinhController } from './thanh-toan-tai-chinh.controller';
 import { ThanhToanTaiChinhService } from './thanh-toan-tai-chinh.service';
@@ -17,7 +20,13 @@ import { ThanhToanController } from './thanh-toan.controller';
 import { ThanhToanService } from './thanh-toan.service';
 
 @Module({
-  imports: [XacThucModule, TonKhoModule, PhanQuyenModule],
+  imports: [
+    XacThucModule,
+    TonKhoModule,
+    PhanQuyenModule,
+    DiemThuongModule,
+    KhuyenMaiModule,
+  ],
   controllers: [
     ThanhToanController,
     ThanhToanCallbackController,
@@ -28,6 +37,7 @@ import { ThanhToanService } from './thanh-toan.service';
     ThanhToanService,
     ThanhToanCallbackService,
     ThanhToanHoanTienService,
+    ThanhToanHoanTienHauXuLyService,
     ThanhToanTaiChinhService,
     MockPaymentGateway,
     VnPaySandboxGateway,
